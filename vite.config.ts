4e8f57e504d386f4e2fbf48388d6982c4e13b19e
Kyoma <diazjeremias159@gmail.com>
2026-08-26 20:18:54 -0300

    fix: update base path in Vite configuration for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/pokemon-type/',
   plugins: [react(), tailwindcss()],
   test: {
     environment: 'node',
